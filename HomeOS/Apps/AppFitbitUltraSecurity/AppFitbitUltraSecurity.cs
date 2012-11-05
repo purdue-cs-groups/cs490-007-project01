1535cbfd68b9b1a9b0474f3c6e2acc7c470ddccc
John Morman <morman@outlook.com>
2012-11-05 17:15:00 -0500

    Updated Security App Code
    Still doesn't work with multisensor

--- HomeOS/Apps/AppFitbitUltraSecurity/AppFitbitUltraSecurity.cs
+++ HomeOS/Apps/AppFitbitUltraSecurity/AppFitbitUltraSecurity.cs
@@ -11,22 +11,31 @@ namespace AppFitbitUltraSecurity
     {
         LogWindow logWindow;
 
-        Dictionary<View.VPort, View.VCapability> otherFitbitUltraPorts;
+        #region Fitbit Ultra Variables
 
-        bool lastResult = true;
+        View.VPort fitbitPort = null;
+        View.VCapability fitbitPortCapability = null;
+
+        #endregion
+
+        #region ZWave Variables
+
+        View.VPort sensorPort = null;
+        View.VCapability sensorPortCapability = null;
+
+        #endregion        
 
         bool homeSecurityActivated = false;
 
         public override void Start()
         {
             logger.Log("Starting {0}", ToString());
- 
-            otherFitbitUltraPorts = new Dictionary<View.VPort, View.VCapability>();
 
             IList<View.VPort> allPortsList = GetAllPortsFromPlatform();
-
-            if (allPortsList != null)
-                ProcessAllPortsList(allPortsList);
+            foreach (View.VPort port in allPortsList)
+            {
+                PortRegistered(port);
+            }
 
             ShowWindow();
 
@@ -66,18 +75,11 @@ namespace AppFitbitUltraSecurity
                 lock (this)
                 {
                     if (logWindow != null)
-                        logWindow.Invoke(new Action(delegate() { logWindow.OtherPortsLog(String.Format("{0} active Fitbit Ultra ports", otherFitbitUltraPorts.Count)); }));
+                        logWindow.Invoke(new Action(delegate() { logWindow.OtherPortsLog(String.Format("{0} active Fitbit Ultra ports", 0)); }));
 
-                    List<View.VPort> ports = otherFitbitUltraPorts.Keys.ToList();
-                    foreach (View.VPort port in ports)
-                    {
-                        RequestCapabilities(port);
-
-                        if (otherFitbitUltraPorts[port] != null)
-                        {
-                            SendGetDevicePresenceMethod(port, otherFitbitUltraPorts[port], counter);
-                        }
-                    }
+                    SendGetDevicePresenceMethod();
+                    SendGetSwitchStatusMethod();
+                   
                 }
 
                 System.Threading.Thread.Sleep(1 * 30 * 1000); // sleep 30 seconds
@@ -86,63 +88,106 @@ namespace AppFitbitUltraSecurity
 
         #region Driver Methods
 
-        public void SendGetDevicePresenceMethod(View.VPort port, View.VCapability capability, int counter)
+        public void SendGetDevicePresenceMethod()
         {
-            IList<View.VParamType> retVals;
-
-            try
+            if (fitbitPort != null && fitbitPortCapability == null)
             {
-                IList<View.VParamType> parameters = new List<View.VParamType>();
-                
-                retVals = port.Invoke(RoleFitbitUltra.RoleName, RoleFitbitUltra.OpGetDevicePresence, parameters, ControlPort, capability, ControlPortCapability);
-            }
-            catch (Exception e)
-            {
-                logger.Log("Error while calling getDevicePresence request: {0}", e.ToString());
-                return;
+                fitbitPortCapability = GetCapability(fitbitPort, Globals.UserSystem);
             }
 
-            bool result = false;
-
-            string message = "";
-            if (retVals[0].Maintype() != (int)ParamType.SimpleType.error)
+            if (fitbitPort != null && fitbitPortCapability != null)
             {
-                result = Convert.ToBoolean(retVals[0].Value());
-                //message = String.Format("{0} success to {1} result = {2}", RoleFitbitUltra.OpGetDevicePresence, port.ToString(), result);
-                if (result == true)
+                IList<View.VParamType> retVals;
+
+                try
                 {
-                    message = String.Format("User is home, and the home security system is {0}", homeSecurityActivated);
-                    if (homeSecurityActivated)
+                    IList<View.VParamType> parameters = new List<View.VParamType>();
+                    parameters.Add(new ParamType(ParamType.SimpleType.binary, "", DateTime.Today, "activityDate"));
+
+                    retVals = fitbitPort.Invoke(RoleFitbitUltra.RoleName, RoleFitbitUltra.OpGetDevicePresence, parameters, ControlPort, fitbitPortCapability, ControlPortCapability);
+                }
+                catch (Exception e)
+                {
+                    logger.Log("Error while calling {0} request: {1}", RoleFitbitUltra.OpGetDevicePresence, e.ToString());
+                    return;
+                }
+
+                bool result;
+
+                string message = "";
+                if (retVals[0].Maintype() != (int)ParamType.SimpleType.error)
+                {
+                    result = Convert.ToBoolean(retVals[0].Value());
+                    if (result == true)
                     {
-                        message = String.Format("{0}\nDeactivating Home security system", message);
-                        homeSecurityActivated = false;
+                        message = String.Format("User is home, and the home security system is {0}", homeSecurityActivated);
+                        if (homeSecurityActivated)
+                        {
+                            message = String.Format("{0}\nDeactivating Home security system", message);
+                            homeSecurityActivated = false;
+                        }
                     }
+                    else
+                    {
+                        message = String.Format("User is Away and the home security system is {0}", homeSecurityActivated);
+                        if (!homeSecurityActivated)
+                        {
+                            message = String.Format("{0}\nActivating Home security system", message);
+                            homeSecurityActivated = true;
+                        }
+                    }
+                } 
+                else
+                {
+                    message = String.Format("{0} failure to {1}", RoleFitbitUltra.OpGetDevicePresence, fitbitPort.ToString());
+                    return;
+                }
+                
+                LogMessageToWindow(message);
+            }
+        }
+
+        public int SendGetSwitchStatusMethod()
+        {
+            if (sensorPort != null && sensorPortCapability == null)
+            {
+                sensorPortCapability = GetCapability(sensorPort, Globals.UserSystem);
+            }
+
+            if (sensorPort != null && sensorPortCapability != null)
+            {
+                IList<View.VParamType> retVals = null;
+
+                try
+                {
+                    IList<View.VParamType> parameters = new List<View.VParamType>();                    
+                    //retVals = sensorPort.Invoke(RoleSwitchBinary.RoleName, RoleSwitchBinary.OpGetName, parameters, ControlPort, sensorPortCapability, ControlPortCapability);
+                    retVals = sensorPort.Invoke(RoleSensor.RoleName, RoleSensor.OpGetName, parameters, ControlPort, sensorPortCapability, ControlPortCapability);
+                }
+                catch (Exception e)
+                {
+                    logger.Log("Error while calling {0} request: {1}", RoleSensor.OpGetName, e.ToString());
+                }
+
+                int result = 0;
+
+                string message = "";
+                if (retVals[0].Maintype() != (int)ParamType.SimpleType.error)
+                {
+                    result = Convert.ToInt32(retVals[0].Value());
+                    message = String.Format("{0} success to {1} result = {2}", RoleSensor.OpGetName, sensorPort.ToString(), result);
                 }
                 else
                 {
-                    message = String.Format("User is Away and the home security system is {0}", homeSecurityActivated);
-                    if (!homeSecurityActivated)
-                    {
-                        message = String.Format("{0}\nActivating Home security system", message);
-                        homeSecurityActivated = true;
-                    }
+                    message = String.Format("{0} failure to {1}", RoleSensor.OpGetName, sensorPort.ToString());
                 }
-            }
-            else
-            {
-                message = String.Format("{0} failure to {1}", RoleFitbitUltra.OpGetDevicePresence, port.ToString());
-                return;
-            }
 
-            LogMessageToWindow(message);
+                LogMessageToWindow(message);
 
-            if (result == true &&
-                lastResult == false)
-            {
-                LogMessageToWindow("User has just entered the scene.");
+                return result;
             }
 
-            lastResult = result;
+            return -1;
         }
 
         public void LogMessageToWindow(string message)
@@ -157,26 +202,6 @@ namespace AppFitbitUltraSecurity
 
         #region Port Management
 
-        public void RequestCapabilities(View.VPort port)
-        {
-            if (otherFitbitUltraPorts[port] == null)
-            {
-                otherFitbitUltraPorts[port] = GetCapability(port, Globals.UserSystem);
-
-                if (otherFitbitUltraPorts[port] != null)
-                {
-                    port.Subscribe(RoleFitbitUltra.RoleName, RoleFitbitUltra.OpGetDevicePresence, ControlPort, otherFitbitUltraPorts[port], ControlPortCapability);
-                }
-            }
-        }
-
-        public void ProcessAllPortsList(IList<View.VPort> portList)
-        {
-            foreach (View.VPort port in portList)
-            {
-                PortRegistered(port);
-            }
-        }
 
         public override void PortRegistered(View.VPort port)
         {
@@ -184,10 +209,19 @@ namespace AppFitbitUltraSecurity
 
             lock (this)
             {
-                if (!otherFitbitUltraPorts.ContainsKey(port) && Role.ContainsRole(port, RoleFitbitUltra.RoleName) && !IsMyPort(port))
+                if (Role.ContainsRole(port, RoleFitbitUltra.RoleName))
                 {
-                    otherFitbitUltraPorts[port] = null;
-                    logger.Log("{0} added port {1}", this.ToString(), port.ToString());
+                    fitbitPort = port;
+                    logger.Log("{0} added fitbitUltra port {1}", this.ToString(), port.ToString());
+
+                    if (logWindow != null)
+                        logWindow.Invoke(new Action(delegate() { logWindow.ConsoleLog("added port " + port.ToString()); }));
+                }
+
+                if (Role.ContainsRole(port, RoleSensor.RoleName))
+                {
+                    sensorPort = port;
+                    logger.Log("{0} added switchbinary port {0}", this.ToString(), port.ToString());
 
                     if (logWindow != null)
                         logWindow.Invoke(new Action(delegate() { logWindow.ConsoleLog("added port " + port.ToString()); }));
@@ -199,10 +233,19 @@ namespace AppFitbitUltraSecurity
         {
             lock (this)
             {
-                if (otherFitbitUltraPorts.ContainsKey(port))
+                if (port.Equals(fitbitPort))
                 {
-                    otherFitbitUltraPorts.Remove(port);
-                    logger.Log("{0} deregistered port {1}", this.ToString(), port.GetInfo().ModuleFacingName());
+                    fitbitPort = null;
+                    logger.Log("{0} removed fitbitUltra port {1}", this.ToString(), port.ToString());
+
+                    if (logWindow != null)
+                        logWindow.Invoke(new Action(delegate() { logWindow.ConsoleLog("removed port " + port.ToString()); }));
+                }
+
+                if (port.Equals(sensorPort))
+                {
+                    sensorPort = null;
+                    logger.Log("{0} removed switchbinary port {0}", this.ToString(), port.ToString());
 
                     if (logWindow != null)
                         logWindow.Invoke(new Action(delegate() { logWindow.ConsoleLog("removed port " + port.ToString()); }));
